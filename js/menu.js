c2800cb383c8eb5b881af926488bd6d56ead84e0
Smekalin Sergei <crypto.mnr@gmail.com>
2020-08-06 21:42:29 +0300

    Закончил хедер
    И кнопка РАБОТАЕТ!

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,23 @@
 (() => {
+  const mobileBtnRef = document.querySelector('[mobile-menu-button]');
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
+    mobileBtnRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+  });
+
+  mobileBtnRef.addEventListener('click', () => {
+    const expanded = mobileBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    mobileBtnRef.classList.toggle('is-open');
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
