659f1ae0c7527f8839882b6365f9d10e390b5c0c
Smekalin Sergei <crypto.mnr@gmail.com>
2020-08-14 10:26:00 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -25,3 +25,24 @@
     document.body.classList.toggle('no-scroll');
   });
 })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu-inner]');
+//   const mobileBackdropRef = document.querySelector('[data-backdrop]');
+//   const body = document.querySelector('body');
+//   const toggleMenu = () => {
+//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//     menuBtnRef.classList.toggle('is-open');
+//     body.classList.toggle('scroll-hidden');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+//     mobileMenuRef.classList.toggle('is-open');
+//     mobileBackdropRef.classList.toggle('is-visible');
+//   };
+//   menuBtnRef.addEventListener('click', () => {
+//     toggleMenu();
+//   });
+//   mobileMenuRef.addEventListener('click', ({ target }) => {
+//     target.getAttribute('data-menu-link') === '' && toggleMenu();
+//   });
+// })();
